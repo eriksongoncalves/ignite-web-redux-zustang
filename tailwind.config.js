c7778ea5af9c55a2395007bd1d65a489c4a127c1
Erikson Gonçalves Bezerra <eriksongoncalves@yahoo.com.br>
2023-06-26 17:45:47 -0300

    feat: player screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [require('tailwind-scrollbar')]
 }
